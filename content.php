7f6dcaa0d5a3761bfa1f025dcc433b3aa777fa76
mdixon47 <mdixon@rap.midco.net>
2014-07-05 16:28:53 -0600

    Added content-none.php and modified content.php

--- content.php
+++ content.php
@@ -32,3 +32,24 @@
 	</header><!-- end entry-header -->
 </article><!-- end article -->
 
+	<!-- Article content -->
+<div class="entry-content">
+	<?php
+	  if (is_search() ) {
+	  		the_excerpt();
+	  } else {
+	  	 the_content( __('Continue reading &rarr;', 'alpha') );
+	  }
+	?>
+</div><!-- end entry-content -->
+<!-- Article footer -->
+<footer class="entry-footer">
+	<?php
+	//  If we have a single page and the author bio exists, display it.
+	if ( is_single() && get_the_author_meta('description') ) {
+		echo '<h2>' . __('Written by' , 'alpha') . get_the_author() . '</h2>';
+		echo '<p>' . the_author_meta('description') . '</p>';
+	}
+	?>
+	
+</footer><!-- end entry-footer -->
